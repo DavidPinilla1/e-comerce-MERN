c8effdcae7c6f49dd6c37abcc1c64b76b8b03a68
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-05-12 12:54:35 +0200

    header connected to the redux store and rendering user depending on the redux state

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './Header.scss';
 import { NavLink } from 'react-router-dom';
-const Header = () => {
+import { connect } from "react-redux";
+const Header = props => {
     return (
         <header className="header">
             {/* NavLink es para crear una clase active cuando esta en la ruta */}
             <NavLink to="/" exact>Home</NavLink>
+            {props.user ?
+                <div className="userZone">
+                    <a>{props.user.email}</a>
+                    <a>Logout</a>
+                </div>
+                :
+                <div className="guestZone">
+                    <NavLink to="/login" exact>Login</NavLink>
+                    <NavLink to="/register" exact>Registro</NavLink>
+                </div>
+            }
 
-            <div className="guestZone">
-                <NavLink to="/login" exact>Login</NavLink>
-                <NavLink to="/register" exact>Registro</NavLink>
-            </div>
         </header>
     )
 }
-export default Header;
+const mapStateToProps = (state) => ({ user: state.user })
+export default connect(mapStateToProps)(Header);
